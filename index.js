53f32947d65f84876be1879a436a6341cc3311d2
Mofolabi Osotimehin <mofolabi.osotimehin@student.moringaschool.com>
2023-01-16 10:51:38 +0100

    done with assignement

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(somevalue){
+    if(somevalue >42){
+        return somevalue - 42;
+    }else{
+        return 42 - somevalue;
+    }
+}
+function distanceFromHqInFeet(somevalue){
+    return distanceFromHqInBlocks(somevalue)*264;
+}
+function distanceTravelledInFeet(start, end){
+    if(end > start){
+        return (end - start) * 264;
+    }else{
+        return (start - end) * 264;
+    }
+}
+function calculatesFarePrice(start, end){
+    let distance = distanceTravelledInFeet(start, end);
+    if(distance < 400) return 0;
+       else if(distance < 2000) return(distance - 400) * 0.02;
+         else if(distance > 2500) return 'cannot travel that far';
+           else return 25
+}
